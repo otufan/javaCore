44a435118c3c6b28caf55f6efc8a90c270a6d248
Omer Tufan <omertufanca@gmail.com>
2021-12-07 22:21:51 +0100

    07.12..21

---
+++ src/day35_encapsulation_inheritance/Personel.java
@@ -0,0 +1,21 @@
+package day35_encapsulation_inheritance;
+
+public class Personel {
+	
+	protected String isim;
+	protected String soyisim;
+	protected boolean calisiyorMu;
+	protected String telNo;
+	protected int yas;
+	
+	//eger variable lara access modifoer yazmazsak Java access modifier olarak default acc. mod. atar
+	//boyle olursa sadece o package altindaki child class lar personel class ini inherit edebilir
+	//baska package larda ki chuild class larin da personel inherit edebilmesi icin variable ve method larin acc mod protected yapariz
+	//publicyapsak ta inherit edilebilir ama biz herkesin erismesi degil de sadece child classlarin erismesini istedigimiz icin protected yapariz
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
