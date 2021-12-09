59186a8a02aabdd2a1b7b669f9de809cd5456386
Omer Tufan <omertufanca@gmail.com>
2021-12-09 21:01:17 +0100

    09.12.21

---
+++ src/day37_inheritence/Araba.java
@@ -0,0 +1,15 @@
+package day37_inheritence;
+
+public class Araba {
+
+		protected int tekerSayisi=4;
+		protected boolean calisiyorMu=true;
+		
+		public void method1() {
+			System.out.println("Araba class indan method 1 calisti");
+		}
+	
+	
+	
+	
+}
---
+++ src/day37_inheritence/Toyota.java
@@ -0,0 +1,21 @@
+package day37_inheritence;
+
+public class Toyota {
+
+	protected boolean pahaliMi;
+	protected String uretimYeri="Japonya";
+	
+	
+	public void method1() {
+		System.out.println("Toyota class indan method1 calisti");
+		
+		
+	}
+	
+	public void method2() {
+		System.out.println("Toyota class indan method2 calisti");
+		
+		
+	}
+	
+}
